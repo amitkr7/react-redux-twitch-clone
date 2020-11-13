0b7b88a4d0193875d997358c4b0deb9447d65d48
Amit <amit840901@gmail.com>
2020-11-14 01:44:24 +0530

    connect create and customize forms

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  // renderInput(formProps) {
+  //   return (
+  //     <input
+  //       onChange={formProps.input.onChange}
+  //       value={formProps.input.value}
+  //     />
+  //   );
+  // }
 
-export default StreamCreate;
+  //Shorter Syntax
+  renderInput({ input, label }) {
+    return (
+      <div className='field'>
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+  render() {
+    return (
+      <form className='ui form'>
+        <Field name='title' component={this.renderInput} label='Enter Title' />
+        <Field
+          name='description'
+          component={this.renderInput}
+          label='Enter Description'
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({ form: 'streamCreate' })(StreamCreate);

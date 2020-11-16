a42859003dfa1703d2dc7503439a12a0fcb0eb8e
Amit <amit840901@gmail.com>
2020-11-16 19:24:27 +0530

    Show Edit,Delete,Create button for logged in user

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,8 +22,11 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  //get the userID from google auth api
+  const { userId } = getState().auth;
+  //post it as an object along with form data
+  const response = await streams.post('/streams', { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -7,10 +8,24 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  //show edit./delete button based on user login
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui  button primary'>Edit</button>
+          <button className='ui  button negative'>Delete</button>
+        </div>
+      );
+    }
+  }
+
+  //show list of available stream
   renderList() {
     return this.props.streams.map((stream) => {
       return (
         <div className='item' key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera'></i>
           <div className='content'>
             {stream.title}
@@ -20,17 +35,36 @@ class StreamList extends React.Component {
       );
     });
   }
+
+  //show create stream button if user is logged in
+  renderCreateButton() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to='/streams/new' className='ui primary button'>
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className='ui celled list'>{this.renderList()}</div>
+        {this.renderCreateButton()}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 export default connect(mapStateToProps, { fetchStreams })(StreamList);

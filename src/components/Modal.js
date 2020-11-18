d1e50b7a81343a7d91de249424556a0b4b4deaed
Amit <amit840901@gmail.com>
2020-11-18 20:09:50 +0530

    create resuable Modal Component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
-const Modal = () => {
-    //Use portal to show modal alongside root element
+const Modal = (props) => {
+  //Use portal to show modal alongside root element
   return ReactDOM.createPortal(
-    <div
-      onClick={() => history.push('/')}
-      className='ui dimmer modals visible active'>
+    <div onClick={props.onDismiss} className='ui dimmer modals visible active'>
       <div
         onClick={(e) => e.stopPropagation()}
         className='ui standard modal visble active'>
-        <div className='header'>Delete Stream</div>
-        <div className='content'>
-          Are you sure you want to delete this stream?
-        </div>
-        <div className='actions'>
-          <buttton className='ui red button'>Delete</buttton>
-          <button className='ui button'>Cancel</button>
-        </div>
+        <div className='header'>{props.title}</div>
+        <div className='content'>{props.content}</div>
+        <div className='actions'>{props.actions}</div>
       </div>
     </div>,
     document.getElementById('modal')
